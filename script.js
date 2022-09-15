65ce9df48db6be3e452af18cb603c224c614e8a8
omarfarukee <umarfaruque1920@gmail.com>
2022-09-15 10:13:01 +0600

    count down hole number fixed

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ const gameOver = () => {
   // the current time is the finish time
   // so total time taken is current time - start time
   const finishTime = new Date().getTime();
-  const timeTaken = (finishTime - startTime) / 1000;
+  const timeTaken = Math.ceil((finishTime - startTime) / 1000);
 
   // show result modal
   resultModal.innerHTML = "";
@@ -134,7 +134,7 @@ displayHistory();
 // Show typing time spent
 setInterval(() => {
   const currentTime = new Date().getTime();
-  const timeSpent = (currentTime - startTime) / 1000;
+  const timeSpent = Math.ceil((currentTime - startTime) / 1000);
 
 
   document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
